3ac490c1b7beb4b4c04d51b2835f810bc0412412
Thanuja Hennayake <108867504+ThanuH@users.noreply.github.com>
2024-09-07 11:01:15 +0530

    Add files via upload
    initial commit

---
+++ client_mangement_system_BE/src/test/java/com/alpha/cms/ClientMangementSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientMangementSystemBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
